f74286373473ac60867cbdcb3c262c9584904b38
Siya2705 <siyamalikofficial@gmail.com>
2025-09-12 14:12:58 +0530

    q8.cpp

--- assignment_2/q8.cpp
+++ assignment_2/q8.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
+#include <unordered_set>
 using namespace std;
 
 int main() {
     int n;
     cout << "Enter number of elements: ";
     cin >> n;
-
     int arr[n];
     cout << "Enter elements:\n";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    int inversionCount = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] > arr[j])
-                inversionCount++;
-        }
+    for(int i=0; i<n; i++) cin >> arr[i];
+    
+    unordered_set<int> distinct;
+    for(int i=0; i<n; i++) {
+        distinct.insert(arr[i]);
     }
-
-    cout << "Total number of inversions: " << inversionCount << endl;
+    
+    cout << "Total distinct elements: " << distinct.size() << endl;
     return 0;
 }
